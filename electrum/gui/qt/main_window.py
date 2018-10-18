53ec4c8eb3e6d35ea11c094bd4fa9f9ce0a82ccd
ThomasV <thomasv@electrum.org>
2018-10-18 13:46:01 +0200

    Qt gui: show messages about payment outcome

--- electrum/gui/qt/main_window.py
+++ electrum/gui/qt/main_window.py
@@ -205,7 +205,7 @@ class ElectrumWindow(QMainWindow, MessageBoxMixin, PrintError):
             interests = ['wallet_updated', 'network_updated', 'blockchain_updated',
                          'new_transaction', 'status',
                          'banner', 'verified', 'fee', 'fee_histogram', 'on_quotes',
-                         'on_history', 'channel', 'channels', 'ln_status']
+                         'on_history', 'channel', 'channels', 'ln_status', 'ln_message']
             # To avoid leaking references to "self" that prevent the
             # window from being GC-ed when closed, callbacks should be
             # methods of this class only, and specifically not be
@@ -318,7 +318,7 @@ class ElectrumWindow(QMainWindow, MessageBoxMixin, PrintError):
             wallet, tx = args
             if wallet == self.wallet:
                 self.tx_notification_queue.put(tx)
-        elif event in ['status', 'banner', 'verified', 'fee', 'fee_histogram']:
+        elif event in ['status', 'banner', 'verified', 'fee', 'fee_histogram', 'ln_message']:
             # Handle in GUI thread
             self.network_signal.emit(event, args)
         elif event == 'on_quotes':
@@ -354,6 +354,10 @@ class ElectrumWindow(QMainWindow, MessageBoxMixin, PrintError):
                 self.do_update_fee()
             # todo: update only unconfirmed tx
             self.history_list.update()
+        elif event == 'ln_message':
+            lnworker, message = args
+            if lnworker == self.wallet.lnworker:
+                self.show_message(message)
         else:
             self.print_error("unexpected network_qt signal:", event, args)
 
